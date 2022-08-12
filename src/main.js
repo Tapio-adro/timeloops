11a9dd6af0534034d6a794c4ec55ea94a777c7bc
Tapio-adro <terentuk4321@gmail.com>
2022-08-12 16:45:15 +0300

    gh-pages

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/css/styles_reset.css';
-import './assets/css/font-awesome-4.7.0/css/font-awesome.min.css';
-import './assets/css/style.css';
-
-createApp(App).mount('#app')
